e82f238caeb41ae21b37110a2dda9a6b90e6f627
a7raser <a7raser@pm.me>
2021-07-27 13:52:12 +0100

    state implemented

--- program/src/state.rs
+++ program/src/state.rs
@@ -12,8 +12,8 @@ pub struct Access {
     pub is_initialized: bool,
     pub hash: [u8; 32],
     pub key: [u8; 32],
+    pub shared_from: Pubkey,
     pub share_limit: u64,
-    pub shared_access: [[u8; 32]; share_limit],
 }
 
 impl Sealed for Access {}
@@ -25,19 +25,13 @@ impl IsInitialized for Access {
 }
 
 impl Pack for Access {
-    const LEN: usize = 73 * (32 * self.share_limit);
+    const LEN: usize = 81;
     fn pack_into_slice(&self, dst: &mut [u8]) {
         dst[0] = self.is_initialized as u8;
         dst[1..33] = self.copy_from_slice(&self.hash);
         dst[33..65] = self.copy_from_slice(&self.key);
-        dst[65..73] = self.copy_from_slice(&self.share_limit.to_le_bytes());
-
-        let mut offset: usize = 73;
-
-        for i in 0..self.share_limit {
-            dst[offset..offset + 32] = self.copy_from_slice(&self.shared_access[i]);
-            offset += 32;
-        }
+        dst[65..73] = self.copy_from_slice(&self.shared_from.to_bytes());
+        dst[73..81] = self.copy_from_slice(&self.share_limit.to_le_bytes());
     }
 
     fn unpack_from_slice(src: &[u8]) -> Result<Self, ProgramError> {
@@ -46,25 +40,17 @@ impl Pack for Access {
             1 => true,
             _ => return Err(ProgramError::InvalidAccountData),
         };
-        let hash: [u8; 32] = src[1..33];
-        let key: [u8; 32] = src[33..65];
-        let share_limit = u64::from_le_bytes(src[65..73]);
-        let shared_access: [[u8; 32]; share_limit] = {
-            let mut offset: usize = 73;
-            let shared_access = Vec::with_capacity(share_limit);
-            for i in 0..share_limit {
-                shared_access.push(src[offset..offset + 32]);
-                offset += 32;
-            }
-            shared_access.collect()
-        };
+        let hash: [u8; 32] = &src[1..33];
+        let key: [u8; 32] = &src[33..65];
+        let shared_from = Pubkey::from_array(&src[65..73]);
+        let share_limit = u64::from_le_bytes(&src[73..81]);
 
         Ok(Self {
             is_initialized,
             hash,
             key,
+            shared_from,
             share_limit,
-            shared_access,
         })
     }
 }
@@ -72,6 +58,7 @@ impl Pack for Access {
 pub struct Owned {
     pub is_initialized: bool,
     pub hash: [u8; 32],
+    pub key: [u8; 32],
     pub value: u64,
     pub share_limit: u64,
 }
@@ -85,14 +72,40 @@ impl IsInitialized for Owned {
 impl Sealed for Owned {}
 
 impl Pack for Owned {
-    fn pack_into_slice(&self, dst: &mut [u8]) {}
-    fn unpack_from_slice(src: &[u8]) -> Result<Self, ProgramError> {}
+    const LEN: usize = 81;
+    fn pack_into_slice(&self, dst: &mut [u8]) {
+        dst[0] = self.is_initialized as u8;
+        dst[1..33].copy_from_slice(&self.hash);
+        dst[33..65].copy_from_slice(&self.key);
+        dst[65..73].copy_from_slice(&self.value.to_le_bytes());
+        dst[73..81].copy_from_slice(&self.share_limit.to_le_bytes());
+    }
+
+    fn unpack_from_slice(src: &[u8]) -> Result<Self, ProgramError> {
+        let is_initialized = match src[0] {
+            0 => false,
+            1 => true,
+            _ => return Err(ProgramError::InvalidAccountData),
+        };
+        let hash: [u8; 32] = &src[1..33];
+        let key: [u8; 32] = &src[33..65];
+        let value = u64::from_le_bytes(&src[65..73]);
+        let share_limit = u64::from_le_bytes(&src[73..81]);
+
+        Ok(Self {
+            is_initialized,
+            hash,
+            key,
+            value,
+            share_limit,
+        })
+    }
 }
 
 pub struct UserState {
-    pub is_initialzed: bool,
-    pub access: [Access; 128],
-    pub owned: [Owned; 128],
+    pub is_initialized: bool,
+    pub access: [Option<Access>; 128],
+    pub owned: [Option<Owned>; 128],
     pub pointer: Option<[u8; 32]>,
 }
 
@@ -105,6 +118,59 @@ impl IsInitialized for UserState {
 impl Sealed for UserState {}
 
 impl Pack for UserState {
-    fn pack_into_slice(&self, dst: &mut [u8]) {}
-    fn unpack_from_slice(src: &[u8]) -> Result<Self, ProgramError> {}
+    const LEN: usize = 20769;
+    const OWNED_BOUND: usize = 20737;
+    const ACCESS_BOUND: usize = 10369;
+
+    fn pack_into_slice(&self, dst: &mut [u8]) {
+        dst[0] = self.is_initialized as u8;
+        dst[1..UserState::ACCESS_BOUND].copy_from_slice(&self.access);
+        dst[UserState::ACCESS_BOUND..UserState::OWNED_BOUND].copy_from_slice(&self.owned);
+        dst[UserState::OWNED_BOUND..UserState::LEN].copy_from_slice(&self.pointer);
+    }
+    fn unpack_from_slice(src: &[u8]) -> Result<Self, ProgramError> {
+        let is_initialized = match src[0] {
+            0 => false,
+            1 => true,
+            _ => return Err(ProgramError::InvalidAccountData),
+        };
+        let access = {
+            let mut offset: usize = 1;
+            let mut access_vec = Vec::with_capacity(128);
+            while offset != UserState::ACCESS_BOUND {
+                let (data, src) = src[offset..UserState::ACCESS_BOUND].split_at(Access::LEN);
+                let access = match Access::unpack_from_slice(data)? {
+                    Access => Some(Access),
+                    _ => None,
+                };
+                access_vec.push(access);
+                offset += Access::LEN;
+            }
+            access_vec.collect()
+        };
+
+        let owned = {
+            let mut offset: usize = UserState::ACCESS_BOUND;
+            let mut owned_vec = Vec::with_capacity(128);
+            while offset != UserState::OWNED_BOUND {
+                let (data, src) = src[offset..UserState::OWNED_BOUND].split_at(Owned::LEN);
+                let owned = match Owned::unpack_from_slice(data)? {
+                    Owned => Some(Owned),
+                    _ => None,
+                };
+                owned_vec.push(owned);
+                offset += Owned::LEN;
+            }
+            owned_vec.collect()
+        };
+
+        let pointer = Pubkey::new_from_array(&src[UserState::OWNED_BOUND..UserState::LEN]);
+
+        Ok(Self {
+            is_initialized,
+            access,
+            owned,
+            pointer,
+        })
+    }
 }
