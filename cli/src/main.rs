abe1cc58da99748c71a1b2bd68b1b1ebaf140792
a7raser <a7raser@pm.me>
2021-07-29 09:24:38 +0100

    cli app implemented

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -5,7 +5,7 @@ use solana_sdk::{
 
 use solana_clap_utils::{
     input_parsers::{pubkey_of, value_of},
-    input_validators::{is_amount, is_keypair, is_parsable, is_pubkey, is_url},
+    input_validators::{is_amount, is_hash, is_keypair, is_parsable, is_pubkey, is_url},
     keypair::DefaultSigner,
 };
 
@@ -169,4 +169,224 @@ fn command_share_access(
     println!("share acess test");
 }
 
-fn main() {}
+fn main() {
+    let app_matches = App::new(crate_name!())
+        .about(crate_description!())
+        .version(crate_version!())
+        .setting(AppSettings::SubcommandRequiredElseHelp)
+        .arg({
+            let arg = Arg::with_name("config_file")
+                .short("c")
+                .long("config")
+                .value_name("PATH")
+                .takes_value(true)
+                .global(true)
+                .help("Configuration file to use");
+            if let Some(ref config_file) = *solana_cli_config::CONFIG_FILE {
+                arg.default_value(&config_file)
+            } else {
+                arg
+            }
+        })
+        .arg(
+            Arg::with_name("rpc_url")
+                .short("u")
+                .long("url")
+                .validator(is_url)
+                .takes_value(true)
+                .global(true)
+                .help("Specifiy target Solana cluster"),
+        )
+        .arg(
+            Arg::with_name("payer")
+                .short("p")
+                .long("payer")
+                .value_name("KEYPAIR")
+                .validator(is_keypair)
+                .takes_value(true)
+                .help(
+                    "Specify payer. \
+                Defaults to client keypair.",
+                ),
+        )
+        .subcommand(
+            Subcommand::with_name("create")
+                .about("Create an account")
+                .arg(
+                    Arg::with_name("account_type")
+                        .short("t")
+                        .long("type")
+                        .value_name("ACCOUNT TYPE")
+                        .takes_value(true)
+                        .required(true)
+                        .index(1)
+                        .help(
+                            "Specify a user account type to create: \
+                    owner \
+                    access \
+                    data",
+                        ),
+                )
+                .arg(
+                    Arg::with_name("hash")
+                        .short("h")
+                        .long("hash")
+                        .validator(is_hash)
+                        .value_name("HASH")
+                        .takes_value(true)
+                        .required(false)
+                        .index(2)
+                        .help("Hash for dataset accounts"),
+                ),
+        )
+        .subcommand(
+            Subcommand::with_name("set")
+                .about("Set dataset parameters")
+                .arg(
+                    Arg::with_name("hash")
+                        .short("h")
+                        .long("hash")
+                        .value_name("HASH")
+                        .validator(is_hash)
+                        .takes_value(true)
+                        .required(true)
+                        .index(1)
+                        .help("Hash of target dataset"),
+                )
+                .arg(
+                    Arg::with_name("key")
+                        .short("k")
+                        .long("key")
+                        .value_name("KEY")
+                        .takes_value(true)
+                        .help("Key for target dataset"),
+                )
+                .arg(
+                    Arg::with_name("value")
+                        .short("v")
+                        .long("value")
+                        .value_name("AMOUNT")
+                        .validator(is_amount)
+                        .takes_value(true)
+                        .help("Value of target dataset in USD"),
+                )
+                .arg(
+                    Arg::with_name("share limit")
+                        .short("l")
+                        .long("share-limit")
+                        .value_name("AMOUNT")
+                        .validator(is_amount)
+                        .takes_value(true)
+                        .help("Number of times the target dataset can be shared"),
+                )
+                .arg(
+                    Arg::with_name("reference_data")
+                        .short("r")
+                        .long("ref-data")
+                        .value_name("PUBKEY")
+                        .validator(is_pubkey)
+                        .takes_value(true)
+                        .help("Address of the dataset the target dataset is derived from"),
+                ),
+        )
+        .subcommand(
+            Subcommand::with_name("purchase_access")
+                .about("Purchase access to a dataset")
+                .arg(
+                    Arg::with_name("hash")
+                        .short("h")
+                        .long("hash")
+                        .value_name("HASH")
+                        .validator(is_hash)
+                        .takes_value(true)
+                        .required(true)
+                        .index(1)
+                        .help("Hash of target dataset"),
+                )
+                .arg(
+                    Arg::with_name("value")
+                        .short("v")
+                        .long("value")
+                        .value_name("AMOUNT")
+                        .validator(is_amount)
+                        .takes_value(true)
+                        .is_required(true)
+                        .index(2)
+                        .help("Value of dataset to purchase access to"),
+                ),
+        )
+        .subcommand(
+            Subcommand::with_name("share_access")
+                .about("Share access to a dataset")
+                .arg(
+                    Arg::with_name("hash")
+                        .short("h")
+                        .long("hash")
+                        .value_name("HASH")
+                        .validator(is_hash)
+                        .takes_value(true)
+                        .required(true)
+                        .index(1)
+                        .help("Hash of the target dataset"),
+                )
+                .arg(
+                    Arg::with_name("recipient")
+                        .short("r")
+                        .long("recipient")
+                        .value_name("PUBKEY")
+                        .validator(is_pubkey)
+                        .takes_value(true)
+                        .required(true)
+                        .index(2)
+                        .help("Address to share access to"),
+                ),
+        )
+        .get_matches();
+
+    let mut wallet_manager = None;
+    let (sub_command, sub_matches) = app_matches.subcommand();
+    let matches = sub_matches.unwrap();
+
+    // Retrieves payer keypair and target RPC from the config file
+    let config = {
+        let cli_config = if let Some(config_file) = matches.value_of("config file") {
+            solana_cli_config::Config::load(config_file).unwrap_or_defaul()
+        } else {
+            solana_cli_config::Config::default()
+        };
+
+        let rpc_url = &cli_config.json_rpc_url;
+        let default_signer_arg_name = "owner".to_string();
+        let default_signer_path = cli_config.keypair_path.clone();
+        let default_signer = DefaultSigner {
+            path: default_signer_path,
+            arg_name: default_signer_arg_name,
+        };
+
+        let payer = {
+            let config = SignerFromPathConfig {
+                allow_null_signer: true,
+            };
+            let payer = default_signer
+                .signer_from_path_with_config(&matches, &mut wallet_manager, &config)
+                .unwrap_or_else(|e| {
+                    eprintln!("error: {}", e);
+                    std::process::exit(1);
+                });
+            payer
+        };
+
+        Config {
+            rpc_client: RpcClient::new(rpc_url.clone()),
+            payer,
+        }
+    };
+
+    match (sub_command, sub_matches) {
+        ("create", Some(args)) => {}
+        ("set", Some(args)) => {}
+        ("purchse_access", Some(args)) => {}
+        ("share_access", Some(args)) => {}
+        _ => unreachable!(),
+    };
+}
